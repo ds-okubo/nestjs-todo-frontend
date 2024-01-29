7ce33f08c3a6e2377003c55f4152226024d31ac9
tatsuki_ookubo <t-okubo@digitalstage.jp>
2024-01-29 11:56:44 +0900

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import { APP_DATA } from '@/constants/appdata';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang={APP_DATA.LOCALE}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument;
